97e96bbe9d52b10b9943f1beba61d533966f5aa9
Christopher Wang <dev@christopher.wang>
2020-01-23 14:20:15 -0800

    Rubocop formatting (#2)
    https://truework.atlassian.net/browse/EN-1259

--- lib/truework.rb
+++ lib/truework.rb
@@ -1,5 +1,8 @@
-require "truework/version"
+# frozen_string_literal: true
 
+require 'truework/version'
+
+# Ruby bindings for the Truework API.
 module Truework
   # Your code goes here...
 end
--- spec/truework_spec.rb
+++ spec/truework_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'truework'
 
 RSpec.describe Truework do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Truework::VERSION).not_to be nil
   end
 end
